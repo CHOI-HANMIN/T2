1e99528319629f73afe132f04b6c7da67fd568c5
CHOI-HANMIN <77436920+CHOI-HANMIN@users.noreply.github.com>
2021-06-06 00:43:55 +0900

    Update and rename gpio_input.py to SW.py

--- SW.py
+++ SW.py
@@ -2,19 +2,19 @@ import RPi.GPIO as GPIO
 import I2C_driver as LCD
 from time import *
 
-mylcd = LCD.lcd()
-Switch= 10
+LCD = LCD.lcd()
+SWC= 10
 LED=11
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(Switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(SWC, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
 while True:
-    mylcd.lcd_clear()
-    if GPIO.input(Switch) == GPIO.HIGH:
-        mylcd.lcd_display_string("LED ON",1)
+    LCD.lcd_clear()
+    if GPIO.input(SWC) == GPIO.HIGH:
+        LCD.lcd_display_string("LED ON",1)
         GPIO.output(LED, GPIO.HIGH)
     else:
-        mylcd.lcd_display_string("LED OFF",2)
+        LCD.lcd_display_string("LED OFF",2)
         GPIO.output(LED, GPIO.LOW)
